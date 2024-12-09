c1f16c63c3592fa2f1d8cdb3e83f1695833e6fe3
povilasgrelis <grepov@gmail.com>
2024-12-09 13:12:58 +0200

    Add user fixtures

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -5,19 +5,26 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixtures extends Fixture
 {
+
+    public function __construct(private UserPasswordHasherInterface $passwordHasher
+    ) {}
+
     public function load(ObjectManager $manager): void
     {
+        $password = 'Secret123';
+
         $adminUser = (new User())
             ->setFirstname('John')
             ->setLastname('Doe')
             ->setEmail('johndoe@example.com')
             ->setPhone('0123456789')
             ->setRoles(['ROLE_ADMIN'])
-            ->setPassword('Secret123')
         ;
+        $adminUser->setPassword($this->passwordHasher->hashPassword($adminUser, $password));
         $manager->persist($adminUser);
 
         $adminUser1 = (new User())
@@ -26,8 +33,8 @@ class UserFixtures extends Fixture
             ->setPhone('0123456789')
             ->setEmail('tomdoe@example.com')
             ->setRoles(['ROLE_ADMIN'])
-            ->setPassword('Secret123')
         ;
+        $adminUser1->setPassword($this->passwordHasher->hashPassword($adminUser1, $password));
         $manager->persist($adminUser1);
 
         $manager->flush();
